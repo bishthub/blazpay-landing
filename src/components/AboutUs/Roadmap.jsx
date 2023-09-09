3adb7b1af3aee759c0ab86767199c47fdf367ef0
anmolbisht2308 <anmolbisht7@gmail.com>
2023-09-10 03:04:42 +0530

    Roadmap completed

--- src/components/AboutUs/Roadmap.jsx
+++ src/components/AboutUs/Roadmap.jsx
@@ -1,155 +1,219 @@
 import React from "react";
 
 import { BsChevronDown } from "react-icons/bs";
-
-const Card = ({
-  title,
-  content,
-  width,
-  height,
-  marginTop,
-  marginBottom,
-  marginRight,
-  marginLeft,
-}) => {
-  const cardStyles = `
- 
-  mx-auto
-  border-2
-  border-yellow-400
-  rounded-md
-  flex
-  items-start
-  flex-col
-`;
-
-  const cardMoreStyles = {
-    width: width === "full" ? "100%" : width,
-    height: height === "full" ? "100%" : height,
-
-    marginBottom: marginBottom,
-    marginTop: marginTop,
-    marginLeft: marginLeft,
-    marginRight: marginRight,
-  };
-
-  const mediaQueryStyles = {
-    marginBottom: marginBottom,
-    marginTop: marginTop,
-  };
-
-  const titleStyles = `
-  mb-2
-  text-base md:text-lg lg:text-xl
-
-  font-semibold
-  h-12
-  bg-gradient-to-r from-red-600 to-yellow-300
-  border-b-2
-  border-orange-500
-  w-full
-  flex
-  flex-row
-  items-center
-  md:p-2 lg-p-4
-
-  md:gap-4 lg:gap-8
-`;
-
-  const contentStyles = `
-  text-xs
-  text-gray-100
-  flex
-  justify-center
-  items-center
-  w-full
-  h-full
-  mx-auto
-  text-center
-  bg-gray-700
-`;
-
-  return (
-    <div
-      data-aos="fade-down"
-      data-aos-duration="1000"
-      data-aos-delay="400"
-      className={cardStyles}
-      style={{
-        ...cardMoreStyles,
-        ...(window.innerWidth <= 768 ? mediaQueryStyles : {}),
-      }}
-    >
-      <h2 className={titleStyles}>
-        <BsChevronDown />
-        {title}
-      </h2>
-      <p className={contentStyles}>{content} </p>
-    </div>
-  );
-};
+import RoadmapImg from "../../assets/roadmap.png";
+import ArrowImg from "../../assets/arrow.png";
+import LeftArrowImg from "../../assets/leftarr.png";
+import LineImg from "../../assets/line.png";
+import Ellipse from "../../assets/ellipsenew.png";
+import Rect from "../../assets/rect.png";
 
 const Roadmap = () => {
   return (
-    <div className="relative flex-col justify-center min-h-screen gap-6 p-20 mt-8 bg-center bg-cover bg-pattern-2">
+    <div className="relative flex-col justify-center w-full min-h-screen gap-6 p-20 mt-8 bg-center bg-cover bg-pattern-2">
       <h1
         data-aos="zoom-in"
         data-aos-easing="linear"
         data-aos-duration="1000"
-        className="mb-6 text-3xl font-semibold  top-5 md:text-5xl md:mb-8"
+        className="mb-6 text-3xl font-semibold top-5 md:text-5xl md:mb-8"
       >
         ROADMAP
       </h1>
+      <div className="relative flex flex-row gap-2">
+        <img src={LineImg} alt="" className="block md:hidden" />
+        <img
+          src={Rect}
+          alt=""
+          className="absolute block w-4 md:hidden -left-2"
+        />
+        <img
+          src={Rect}
+          alt=""
+          className="absolute bottom-0 block w-4 md:hidden -left-2"
+        />
 
-      <div className="relative flex flex-col justify-center w-full gap-10 pl-10 md:flex-row margin-1 g-8 md:pl-5 lg:pl-30 xl:pl-40">
-        <Card
-          title="Step 1"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum erat ut tristique."
-          width="15rem"
-          height="15rem"
-          marginTop=""
-        />
-        <Card
-          title="Step 2"
-          content="Vestibulum vehicula purus ut ante ullamcorper, a convallis nulla fermentum."
-          width="18rem"
-          height="20rem"
-          marginTop="5rem"
-        />
-        <Card
-          title="Step 3"
-          content="Vestibulum vehicula purus ut ante ullamcorper, a convallis nulla fermentum."
-          width="15rem"
-          height="15rem"
-          marginTop="20rem"
-        />
-      </div>
-      <div className="flex flex-col justify-center gap-4 m-4 md:gap-10 md:flex-row margin-1 lg:g-8">
-        <Card
-          title="Step 4"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum erat ut tristique."
-          width="20rem"
-          height="23rem"
-          marginTop="-15rem"
-          marginLeft=""
-          marginRight=""
-        />
-        <Card
-          title="Step 5"
-          content="Vestibulum vehicula purus ut ante ullamcorper, a convallis nulla fermentum."
-          width="13rem"
-          height="21rem"
-          marginTop="-10rem"
-          marginBottom="0"
-        />
-        <Card
-          title="Step 6"
-          content="Vestibulum vehicula purus ut ante ."
-          width="15rem"
-          height="13rem"
-          marginTop=""
-          marginBottom=""
-        />
+        <div className="relative flex flex-col justify-center w-full gap-4 m-1 lg:gap-10 g-8 ">
+          <div className="flex flex-col items-center justify-center w-full gap-4 lg:flex-row sm:gap-2 lg:gap-10">
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md lg:h-60"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+
+              // style={{ height: "15rem" }}
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q4 2023</h1>
+              <ul className="pl-6 list-disc">
+                <li>Landing</li>
+                <li>WebsiteEntrypasses</li>
+                <li>Beta V1 User account</li>
+              </ul>
+            </div>
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md lg:h-60"
+              data-aos="fade-down"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q2 2024</h1>
+              <ul className="pl-6 list-disc">
+                <li>Beta V2 – Merchant account</li>
+                <li>Beta Dedicated Payment gateways for leading chains</li>
+                <li>Mainnet Dedicated Payment gateways for leading chains</li>
+                Main net – Blazpay (tokenlaunch)
+              </ul>
+            </div>
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md lg:h-60"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+              // style={{ height: "15rem" }}
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q2 2024</h1>
+              <ul className="pl-6 list-disc">
+                <li>Beta web3 domain</li>
+                <li>web3 Domain</li>
+              </ul>
+            </div>
+          </div>
+          <div className="relative flex items-center justify-center w-full">
+            <img className="hidden w-full md:block" src={RoadmapImg} alt="" />
+            <img
+              src={ArrowImg}
+              alt=""
+              className="absolute top-0 left-0 hidden md:block "
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={ArrowImg}
+              alt=""
+              className="absolute top-0 hidden left-1/4 md:block"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={ArrowImg}
+              alt=""
+              className="absolute top-0 hidden left-1/2 md:block"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={ArrowImg}
+              alt=""
+              className="absolute top-0 hidden left-3/4 md:block"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={LeftArrowImg}
+              alt=""
+              className="absolute bottom-0 left-0 hidden md:block"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={LeftArrowImg}
+              alt=""
+              className="absolute bottom-0 hidden left-1/4 md:block"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={LeftArrowImg}
+              alt=""
+              className="absolute bottom-0 hidden left-1/2 md:block"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+            <img
+              src={LeftArrowImg}
+              alt=""
+              className="absolute bottom-0 hidden left-3/4 md:block"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center w-full gap-4 lg:flex-row sm:gap-2 lg:gap-10 ">
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md lg:h-32"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q4 2023</h1>
+              <ul className="pl-6 list-disc">
+                <li>Beta BLAZNFT buying solution</li>
+                <li>BLAZNFT buying solution</li>
+              </ul>
+            </div>
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md md-h-full lg:h-32"
+              data-aos="fade-down"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+              // style={{ height: "8rem" }}
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q4 2023</h1>
+              <ul className="pl-6 list-disc">
+                <li>Beta BLAZSwap</li>
+                <li>BLAZSwap</li>
+              </ul>
+            </div>
+            <div
+              className="flex flex-col justify-start w-full h-full p-2 border border-orange-700 rounded-lg backdrop-blur-md lg:h-32"
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="400"
+            >
+              <img
+                src={Ellipse}
+                alt=""
+                className="absolute block w-8 -left-8 md:hidden"
+              />
+              <h1>Q4 2023</h1>
+              <ul className="pl-6 list-disc">
+                <li>Beta MOBILE APPLICATION</li>
+                <li>Mobile application</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
