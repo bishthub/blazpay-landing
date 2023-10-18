7421a5aec5fa78eedcec03018c8d945c0085d6a9
bishthub <1803013103@ipec.org.in>
2023-10-18 21:04:10 +0530

    added chain

--- src/components/ClaimEntryPass/ClaimMint.jsx
+++ src/components/ClaimEntryPass/ClaimMint.jsx
@@ -1,12 +1,87 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ethers } from "ethers";
+import arbAbi from "./arbAbi.json";
+import omniAbi from "./omniAbi.json";
 
-const MainNetCard = ({ entryPass }) => {
+const connectWallet = async (chain) => {
+  if (window.ethereum) {
+    try {
+      // Request account access
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const network = await provider.getNetwork();
+      if (chain === "omni") {
+        if (network.chainId !== 165) {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0xA5",
+                chainName: "Omni Testnet",
+                nativeCurrency: { name: "OMNI", symbol: "OMNI", decimals: 18 },
+                rpcUrls: ["https://testnet.omni.network"], // replace with the actual RPC
+                blockExplorerUrls: ["https://testnet.explorer.omni.network"], // replace with the actual block explorer
+              },
+            ],
+          });
+        }
+      } else if (chain === "arbitrum") {
+        if (network.chainId !== 421613) {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x66EED",
+                chainName: "Arbitrum Goerli",
+                nativeCurrency: { name: "ETH", symbol: "ETH", decimals: 18 },
+                rpcUrls: ["https://goerli-rollup.arbitrum.io/rpc"], // replace with the actual RPC
+                blockExplorerUrls: ["https://goerli.arbiscan.io/"], // replace with the actual block explorer
+              },
+            ],
+          });
+        }
+      } else {
+        console.log("ERROR IN CHAIN");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+const ArbCard = ({ entryPass }) => {
+  const mintTokenArbitrum = async () => {
+    if (window.ethereum) {
+      try {
+        await connectWallet("arbitrum"); // ensure wallet is connected and on the right network
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        // replace with your contract's ABI
+        const contractAbi = arbAbi;
+        const contract = new ethers.Contract(
+          "0x9903A7aBf4223743946AB79357A2A9051393a2Df",
+          contractAbi,
+          signer
+        );
+        const tx = await contract.increment();
+        console.log("Transaction Sent:", tx.hash);
+        await tx.wait(); // wait for transaction to be mined
+        console.log("Transaction Mined:", tx.hash);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div className="flex items-center justify-center w-full">
       <div className="flex flex-col items-center justify-center w-1/2 gap-4 p-5 m-2  bg-gradient-to-r from-[#FF3503] to-yellow-500 rounded-lg">
         <h1>{entryPass.name}</h1>
-        <button className="flex items-center justify-center px-10 py-2 text-black bg-white rounded-lg">
+        <button
+          onClick={mintTokenArbitrum}
+          className="flex items-center justify-center px-10 py-2 text-black bg-white rounded-lg"
+        >
           Mint Now
         </button>
       </div>
@@ -14,12 +89,84 @@ const MainNetCard = ({ entryPass }) => {
   );
 };
 
-const TestNetCard = ({ entryPass }) => {
+const OmniCard = ({ entryPass }) => {
+  const mintTokenOmni = async () => {
+    if (window.ethereum) {
+      try {
+        await connectWallet("omni"); // ensure wallet is connected and on the right network
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        // replace with your contract's ABI
+        const contractAbi = omniAbi;
+        const contract = new ethers.Contract(
+          "0xbC1CD6d23fb31fAc1b2BC10a048f00Bd13B259E4",
+          contractAbi,
+          signer
+        );
+        const overrides = {
+          gasLimit: ethers.utils.parseUnits("250000", "wei"), // you may need to adjust this value
+          value: ethers.utils.parseEther("1"),
+        };
+        const tx = await contract.mintNFT(overrides);
+        console.log("Transaction Sent:", tx.hash);
+        await tx.wait(); // wait for transaction to be mined
+        console.log("Transaction Mined:", tx.hash);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div className="w-full">
       <div className="flex flex-col items-center justify-center w-1/2 gap-4 p-5 m-2 bg-blue-500 rounded-lg">
         <h1>{entryPass.name}</h1>
-        <button className="flex items-center justify-center px-10 py-2 text-black bg-white rounded-lg">
+        <button
+          onClick={mintTokenOmni}
+          className="flex items-center justify-center px-10 py-2 text-black bg-white rounded-lg"
+        >
+          Mint Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const Omni1Card = ({ entryPass }) => {
+  const mintTokenOmni = async () => {
+    if (window.ethereum) {
+      try {
+        await connectWallet("omni"); // ensure wallet is connected and on the right network
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        // replace with your contract's ABI
+        const contractAbi = omniAbi;
+        const contract = new ethers.Contract(
+          "0xbC1CD6d23fb31fAc1b2BC10a048f00Bd13B259E4",
+          contractAbi,
+          signer
+        );
+        const overrides = {
+          gasLimit: ethers.utils.parseUnits("250000", "wei"), // you may need to adjust this value
+        };
+        const tx = await contract.mintNFT(overrides);
+        console.log("Transaction Sent:", tx.hash);
+        await tx.wait(); // wait for transaction to be mined
+        console.log("Transaction Mined:", tx.hash);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-col items-center justify-center w-1/2 gap-4 p-5 m-2 bg-blue-500 rounded-lg">
+        <h1>{entryPass.name}</h1>
+        <button
+          onClick={mintTokenOmni}
+          className="flex items-center justify-center px-10 py-2 text-black bg-white rounded-lg"
+        >
           Mint Now
         </button>
       </div>
@@ -33,9 +180,10 @@ const ClaimMint = () => {
 
   useEffect(() => {
     axios
-      .get("http://34.93.103.113:3000/api/entrypass/entry-passes")
+      .get("http://localhost:3000/api/entrypass/entry-passes")
       .then((response) => {
         setEntryPassData(response.data);
+        console.log("response", response.data);
       });
   }, []);
 
@@ -50,16 +198,6 @@ const ClaimMint = () => {
           <button
             onClick={HandleSelect}
             className={`rounded-2xl px-5 text-lg ${
-              select
-                ? "bg-gradient-to-r from-[#FF3503] to-yellow-500"
-                : "bg-white text-black"
-            }`}
-          >
-            MainNet
-          </button>
-          <button
-            onClick={HandleSelect}
-            className={`rounded-2xl px-5 text-lg ${
               !select
                 ? "bg-gradient-to-r from-[#FF3503] to-yellow-500"
                 : "bg-white text-black"
@@ -67,15 +205,32 @@ const ClaimMint = () => {
           >
             TestNet
           </button>
+          <button
+            onClick={HandleSelect}
+            className={`rounded-2xl px-5 text-lg ${
+              select
+                ? "bg-gradient-to-r from-[#FF3503] to-yellow-500"
+                : "bg-white text-black"
+            }`}
+          >
+            MainNet
+          </button>
         </div>
         <div className="grid justify-center w-full grid-cols-1 m-auto md:grid-cols-2">
           {entryPassData.map((entryPass) => {
-            // Conditionally render cards based on the type
+            // Conditionally render cards based on the type and chain
             if (select && entryPass.type === "mainnet") {
               return <MainNetCard key={entryPass._id} entryPass={entryPass} />;
             } else if (!select && entryPass.type === "testnet") {
-              return <TestNetCard key={entryPass._id} entryPass={entryPass} />;
+              if (entryPass.chain === "Omni") {
+                return <OmniCard key={entryPass._id} entryPass={entryPass} />;
+              } else if (entryPass.chain === "Arbitrum") {
+                return <ArbCard key={entryPass._id} entryPass={entryPass} />;
+              } else if (entryPass.chain === "Omni1") {
+                return <Omni1Card key={entryPass._id} entryPass={entryPass} />;
+              }
             }
+            console.log("ENTRY PASS DATA ", entryPassData);
             return null; // Skip rendering if type doesn't match
           })}
         </div>
