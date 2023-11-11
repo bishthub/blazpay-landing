05945b541109681e84d9a8c4cf3f07c2bc0115fc
bishthub <1803013103@ipec.org.in>
2023-11-12 02:02:01 +0530

    removed chains

--- src/components/ClaimEntryPass/Claim.jsx
+++ src/components/ClaimEntryPass/Claim.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Navbar from "../Navbar";
-import ClaimHero from "./ClaimHero";
-import Gradient from "../../utilities/Gradient";
-import ClaimMint from "./ClaimMint";
-import Footer from "../Footer";
+import React from 'react';
+import Navbar from '../Navbar';
+import ClaimHero from './ClaimHero';
+import Gradient from '../../utilities/Gradient';
+import ClaimMint from './ClaimMint';
+import Footer from '../Footer';
 
 const Claim = () => {
   return (
-    <div className="w-full bg-black">
+    <div className='w-full bg-black'>
       <Navbar />
       <ClaimHero />
       <Gradient />
-      <ClaimMint />
+      {/* <ClaimMint /> */}
       <Footer />
     </div>
   );
