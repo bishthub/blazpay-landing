fe9266136020be6e1eeacc0f7f4c96b60d24c309
bishthub <1803013103@ipec.org.in>
2023-12-20 21:24:59 +0530

    made changes to zeta pass URI

--- src/components/ClaimEntryPass/ClaimZeta.jsx
+++ src/components/ClaimEntryPass/ClaimZeta.jsx
@@ -67,6 +67,26 @@ const connectWallet = async (chain) => {
             ],
           });
         }
+      } else if (chain === 'zeta') {
+        if (network.chainId !== 7001) {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x1b59',
+                chainName: 'ZetaChain Athens 3 Testnet',
+                nativeCurrency: {
+                  name: 'ZETA',
+                  symbol: 'ZETA',
+                  decimals: 18,
+                },
+                rpcUrls: [
+                  'https://zetachain-athens-evm.blockpi.network/v1/rpc/public',
+                ], // replace with the actual RPC
+              },
+            ],
+          });
+        }
       } else {
         await window.ethereum.request({ method: 'eth_requestAccounts' });
       }
@@ -93,7 +113,7 @@ const ZetaCard = ({ entryPass, imgSrc, onMintSuccess }) => {
           );
 
           const contract = new ethers.Contract(
-            '0x7ea13d779369e3b0b5F78cB050aD97DE6cA9CfBD',
+            '0x8001a63FAbc3D58800633fa584F267B9D6b5dEe5',
             goerliAbi,
             signer
           );
@@ -119,7 +139,7 @@ const ZetaCard = ({ entryPass, imgSrc, onMintSuccess }) => {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         const contract = new ethers.Contract(
-          '0x7ea13d779369e3b0b5F78cB050aD97DE6cA9CfBD', // Replace with your contract address
+          '0x8001a63FAbc3D58800633fa584F267B9D6b5dEe5', // Replace with your contract address
           goerliAbi, // Replace with the correct ABI that includes getOwnedTokens
           signer
         );
@@ -147,7 +167,7 @@ const ZetaCard = ({ entryPass, imgSrc, onMintSuccess }) => {
         const contractAbi = goerliAbi;
         const address = await signer.getAddress();
         const contract = new ethers.Contract(
-          '0x7ea13d779369e3b0b5F78cB050aD97DE6cA9CfBD',
+          '0x8001a63FAbc3D58800633fa584F267B9D6b5dEe5',
           contractAbi,
           signer
         );
@@ -228,7 +248,7 @@ const MaticCard = ({ entryPass, imgSrc, tokenId }) => {
           );
 
           const contract = new ethers.Contract(
-            '0x0768BDD56A00Fe7E18A91033d4b84Ddbb74d0201',
+            '0xA3E882aC208d729ad63A0064af33b06B1E7EDF02',
             goerliAbi,
             signer
           );
@@ -256,7 +276,7 @@ const MaticCard = ({ entryPass, imgSrc, tokenId }) => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const contract = new ethers.Contract(
-          '0x7ea13d779369e3b0b5F78cB050aD97DE6cA9CfBD',
+          '0x8001a63FAbc3D58800633fa584F267B9D6b5dEe5',
           goerliAbi,
           signer
         );
@@ -320,6 +340,146 @@ const MaticCard = ({ entryPass, imgSrc, tokenId }) => {
   );
 };
 
+const ZetaBasic = ({ entryPass, imgSrc, onMintSuccess }) => {
+  const [userBalance, setUserBalance] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchUserBalance = async () => {
+      if (window.ethereum) {
+        try {
+          await connectWallet('zeta');
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const address = await signer.getAddress();
+          console.log(
+            '🚀 ~ file: ClaimZeta.jsx:90 ~ fetchUserBalance ~ address:',
+            address
+          );
+
+          const contract = new ethers.Contract(
+            '0x4dE7CD522f1715b2a48F3ad6612924841d450A0F',
+            goerliAbi,
+            signer
+          );
+          const ownedTokenIds = await contract.balanceOf(address);
+          console.log('Owned Token IDs:', ownedTokenIds);
+          const balance = await contract.balanceOf(address);
+          console.log(
+            '🚀 ~ file: ClaimZeta.jsx:96 ~ fetchUserBalance ~ balance:',
+            balance
+          );
+          setUserBalance(balance.toString());
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    fetchUserBalance();
+  }, []);
+  const fetchOwnedTokenIds = async () => {
+    if (window.ethereum) {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        const contract = new ethers.Contract(
+          '0x4dE7CD522f1715b2a48F3ad6612924841d450A0F', // Replace with your contract address
+          zetaAbi, // Replace with the correct ABI that includes getOwnedTokens
+          signer
+        );
+
+        // Fetching the owned token IDs
+        const ownedTokenIds = await contract.balanceOf(address);
+        console.log('Owned Token IDs:', ownedTokenIds);
+
+        // You can use the last token ID here or pass it to other functions
+        if (ownedTokenIds.length > 0) {
+          const lastTokenId = ownedTokenIds[ownedTokenIds.length - 1];
+          onMintSuccess(lastTokenId); // Passing the last token ID to the parent component
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  const mintTokenZeta = async () => {
+    if (window.ethereum) {
+      try {
+        await connectWallet('zeta'); // ensure wallet is connected and on the right network
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contractAbi = zetaAbi;
+        const address = await signer.getAddress();
+        const contract = new ethers.Contract(
+          '0x4dE7CD522f1715b2a48F3ad6612924841d450A0F',
+          contractAbi,
+          signer
+        );
+        const mintPrice = ethers.utils.parseEther('1'); // Replace '0.5' with the actual mint price
+        const overrides = {
+          gasLimit: ethers.utils.hexlify(250000), // Adjust gas limit as needed
+          value: mintPrice, // ETH amount for minting
+        };
+        const tx = await contract.safeMint(address, overrides);
+
+        // Wait for the transaction to be mined
+        await tx.wait();
+        const receipt = await provider.getTransactionReceipt(tx.hash);
+
+        // Create an Interface object to decode logs
+        const contractInterface = new ethers.utils.Interface(contractAbi);
+        let mintedTokenId;
+        // Parse logs to find the BlazpayXZetaEvent
+        receipt.logs.forEach((log) => {
+          try {
+            const parsedLog = contractInterface.parseLog(log);
+            if (parsedLog.name === 'BlazpayXZetaEvent') {
+              console.log('BlazpayXZetaEvent:', parsedLog.args);
+              mintedTokenId = parsedLog.args[0];
+            }
+          } catch (error) {
+            // This will catch if log is not from our contract
+          }
+        });
+        if (mintedTokenId !== undefined) {
+          onMintSuccess(mintedTokenId.toString()); // Call the callback with the minted tokenId
+        }
+
+        toast.success(
+          'Mint Successful, now you have access to Blazpay Dashboard',
+          {
+            onClose: () => window.location.reload(),
+          }
+        );
+        console.log('Transaction Hash:', tx.hash);
+        await fetchOwnedTokenIds();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className=" flex flex-col items-center justify-center gap-4 p-5 m-2 rounded-lg">
+        <img
+          style={{ borderRadius: '5%', maxWidth: '40%' }}
+          src={imgSrc}
+          alt="zeta"
+        />
+        <h1 className="text-xl text-center">Zeta X Blazpay Entry Pass</h1>
+        <h2>You Own {userBalance}</h2>
+        <button
+          onClick={mintTokenZeta}
+          className="flex items-center justify-center px-10 py-2 bg-gradient-to-r from-[#FF3503] to-yellow-500 font-bold rounded-lg"
+        >
+          Get It Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const ClaimZeta = () => {
   const [select, setSelect] = useState(false);
   const [entryPassData, setEntryPassData] = useState([]);
@@ -338,7 +498,7 @@ const ClaimZeta = () => {
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         const contract = new ethers.Contract(
-          '0x7ea13d779369e3b0b5F78cB050aD97DE6cA9CfBD',
+          '0x8001a63FAbc3D58800633fa584F267B9D6b5dEe5',
           arbAbi,
           signer
         );
@@ -430,11 +590,36 @@ const ClaimZeta = () => {
                 2. After you have approved the transaction twice you will be
                 able to see the Mumbai x Zeta Entry Pass after 15 minutes.
               </p>
+              <br />
+              <p className="text-white font-semibold">
+                3. Mint cross chain entry pass and get double blazTokens on
+                Blazpay dashboard for every transaction that you make.
+              </p>
             </div>
           </div>
         </div>
 
-        {}
+        <div className="w-full flex justify-center items-center m-10">
+          <h1 style={{ fontSize: '3rem', fontWeight: '900' }}>OR</h1>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center items-center">
+          {entryPassData.map((entryPass) => {
+            if (!select && entryPass.type === 'testnet') {
+              if (entryPass.chain === 'Zeta') {
+                return (
+                  <div className="flex w-full justify-center items-center flex-col md:flex-row ">
+                    <ZetaBasic
+                      key={entryPass._id}
+                      entryPass={entryPass}
+                      imgSrc="/zeta.jpg"
+                    />
+                  </div>
+                );
+              }
+            }
+          })}
+        </div>
       </div>
     </div>
   );
